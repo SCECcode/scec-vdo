48c6b2d102491a6d88def152f4cfa2d68910dafa
sanskriti <sanskriti@25aa0b20-aa2b-420e-afc3-456cccd12aca>
2016-03-21 19:52:49 +0000

    revision 1 - note: need to restart the application when importing cfm for first time
    git-svn-id: http://source.usc.edu/svn/scec_vdo_vtk@1 25aa0b20-aa2b-420e-afc3-456cccd12aca

---
+++ src/org/scec/vtk/main/Info.java
@@ -0,0 +1,17 @@
+package org.scec.vtk.main;
+
+
+
+public class Info {
+	private static MainGUI mainGUI;
+
+	public static MainGUI getMainGUI() {
+		 
+		return mainGUI;
+	}
+
+	public static void setMainGUI(MainGUI mgui) {
+		// TODO Auto-generated method stub
+		mainGUI = mgui;
+	}
+}
