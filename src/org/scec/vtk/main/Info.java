557a1e31e8f855bb5db6b7a92c83ae4c152bdeff
sanskriti <sanskriti@25aa0b20-aa2b-420e-afc3-456cccd12aca>
2016-03-30 22:57:58 +0000

    Updated grid plugin GUI
    git-svn-id: http://source.usc.edu/svn/scec_vdo_vtk@6 25aa0b20-aa2b-420e-afc3-456cccd12aca

--- src/org/scec/vtk/main/Info.java
+++ src/org/scec/vtk/main/Info.java
@@ -1,6 +1,6 @@
 package org.scec.vtk.main;
 
-
+import java.awt.Color;
 
 public class Info {
 	private static MainGUI mainGUI;
@@ -13,5 +13,13 @@ public class Info {
 	public static void setMainGUI(MainGUI mgui) {
 		// TODO Auto-generated method stub
 		mainGUI = mgui;
+	}
+	public static double[] convertColor(Color tempColor)
+	{
+		double[] color = new double[3];
+		color[0] = tempColor.getRed()/rgbMax;
+		color[1] = tempColor.getGreen()/rgbMax;
+		color[2] = tempColor.getBlue()/rgbMax;
+		return color;
 	}
 }
