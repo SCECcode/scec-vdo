4626e095f48d20892d6734937e0b545c1e24fd8d
sanskriti <sanskriti@25aa0b20-aa2b-420e-afc3-456cccd12aca>
2016-04-11 23:00:30 +0000

    added scripting plugin gui
    git-svn-id: http://source.usc.edu/svn/scec_vdo_vtk@11 25aa0b20-aa2b-420e-afc3-456cccd12aca

---
+++ trunk/plugins/ScriptingPlugin/src/org/scec/vtk/plugins/ScriptingPlugin/KeyFrame.java
@@ -0,0 +1,26 @@
+package org.scec.vtk.plugins.ScriptingPlugin;
+
+import org.scec.vtk.main.Info;
+
+import vtk.vtkCamera;
+import vtk.vtkPoints;
+
+public class KeyFrame {
+
+ double[] camPos;
+ vtkCamera cam ;
+ public KeyFrame() {
+	// TODO Auto-generated constructor stub
+	 camPos = new double[3];
+	 cam = Info.getMainGUI().getRenderWindow().GetRenderer().GetActiveCamera();
+	 camPos = cam.GetPosition();
+}
+ public double[] getCamPos()
+ {
+	 return camPos;
+ }
+ public void setCamPos(double[] campos)
+ {
+	 this.camPos = campos;
+ }
+}
---
+++ trunk/plugins/ScriptingPlugin/src/org/scec/vtk/plugins/ScriptingPlugin/ScriptingPlugin.java
@@ -0,0 +1,33 @@
+package org.scec.vtk.plugins.ScriptingPlugin;
+
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+import org.scec.vtk.drawingTools.DrawingToolsGUI;
+import org.scec.vtk.plugins.ActionPlugin;
+
+import vtk.vtkActor;
+
+public class ScriptingPlugin  extends ActionPlugin {
+	ScriptingPluginGUI gratPanel;
+
+	public ScriptingPlugin() {
+		// this.metadata = new PluginInfo("Grids", "Grids", "David & Genia",
+		// "1.0");
+	}
+
+	public JPanel createGUI() {
+		this.gratPanel = new ScriptingPluginGUI(this);
+		return this.gratPanel;
+	}
+
+	public ScriptingPluginGUI getGraticuleGUI() {
+		return gratPanel;
+	}
+
+	public ArrayList<vtkActor> getActors() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
