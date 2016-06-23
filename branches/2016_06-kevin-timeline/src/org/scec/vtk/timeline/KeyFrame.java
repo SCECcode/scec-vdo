795cb3730d2fcbdd04fb2fb78605427619f01750
kmilner <kmilner@25aa0b20-aa2b-420e-afc3-456cccd12aca>
2016-06-23 17:50:36 +0000

    initial keyframe classes
    git-svn-id: http://source.usc.edu/svn/scec_vdo_vtk@102 25aa0b20-aa2b-420e-afc3-456cccd12aca

---
+++ branches/2016_06-kevin-timeline/src/org/scec/vtk/timeline/KeyFrame.java
@@ -0,0 +1,37 @@
+package org.scec.vtk.timeline;
+
+import org.scec.vtk.plugins.PluginState;
+
+import com.google.common.base.Preconditions;
+
+public class KeyFrame implements Comparable<KeyFrame> {
+	
+	private double startTime;
+	
+	private PluginState state;
+	
+	public KeyFrame(double startTime, PluginState state) {
+		Preconditions.checkArgument(startTime >= 0);
+		Preconditions.checkNotNull(state);
+		this.startTime = startTime;
+		this.state = state;
+	}
+	
+	public PluginState getState() {
+		return state;
+	}
+	
+	public void load() {
+		state.load();
+	}
+
+	@Override
+	public int compareTo(KeyFrame o) {
+		return Double.compare(startTime, o.startTime);
+	}
+	
+	public double getStartTime() {
+		return startTime;
+	}
+
+}
