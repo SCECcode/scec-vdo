f42550f616908d0086521a92c50b988381cc4fe7
pvanderw <pvanderw@25aa0b20-aa2b-420e-afc3-456cccd12aca>
2016-06-30 16:50:43 +0000

    Added ability to double click faults to display information about that fault section in a pop-up window.
    git-svn-id: http://source.usc.edu/svn/scec_vdo_vtk@131 25aa0b20-aa2b-420e-afc3-456cccd12aca
    
    Former-commit-id: b9863b7d43fd9775e5ffcc0699590fc578639c58

--- trunk/commons/src/org/scec/vtk/commons/opensha/surfaces/pickBehavior/NameDispalyPickHandler.java
+++ trunk/commons/src/org/scec/vtk/commons/opensha/surfaces/pickBehavior/NameDispalyPickHandler.java
@@ -2,9 +2,12 @@ package org.scec.vtk.commons.opensha.surfaces.pickBehavior;
 
 import java.awt.event.MouseEvent;
 
+import javax.swing.JOptionPane;
+
 import org.scec.vtk.commons.opensha.faults.AbstractFaultSection;
 import org.scec.vtk.commons.opensha.faults.colorers.CPTBasedColorer;
 import org.scec.vtk.commons.opensha.faults.colorers.FaultColorer;
+import org.scec.vtk.main.Info;
 import org.scec.vtk.tools.picking.PickEnabledActor;
 import org.scec.vtk.tools.picking.PickHandler;
 
@@ -21,7 +24,7 @@ public class NameDispalyPickHandler implements PickHandler<AbstractFaultSection>
 	@Override
 	public void actorPicked(PickEnabledActor<AbstractFaultSection> actor,
 			AbstractFaultSection fault, vtkCellPicker picker, MouseEvent e) {
-		if (fault == null || e.getButton() != MouseEvent.BUTTON1)
+		if (fault == null || e.getButton() != MouseEvent.BUTTON1 || e.getClickCount() < 2 || e.getClickCount() > 2)
 			return;
 		String s = fault.getInfo();
 		if (this.colorer != null && this.colorer instanceof CPTBasedColorer) {
@@ -30,8 +33,10 @@ public class NameDispalyPickHandler implements PickHandler<AbstractFaultSection>
 				s = "Val: "+(float)val+"\n"+s;
 			} catch (Exception ex) {}
 		}
-		s = s.replaceAll("\n", ", ");
-		System.out.println(s);
+		
+		JOptionPane.showMessageDialog(Info.getMainGUI(), s, "Fault Section Information", JOptionPane.INFORMATION_MESSAGE);
+//		s = s.replaceAll("\n", ", ");
+//		System.out.println(s);
 		// TODO show in GUI
 	}
 
