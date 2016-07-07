128888bd777d9785fe80800797670cba18db65bc
kishan <kishan@25aa0b20-aa2b-420e-afc3-456cccd12aca>
2016-07-07 17:19:05 +0000

    Automated the loading of the local shake map files in the data/ShakeMapPlugin directory.
    git-svn-id: http://source.usc.edu/svn/scec_vdo_vtk@155 25aa0b20-aa2b-420e-afc3-456cccd12aca
    
    Former-commit-id: 38c6b824d22be4fa17c9a691d27ba2b3e31238eb

--- trunk/plugins/ShakeMapPlugin/src/org/scec/vtk/plugins/ShakeMapPlugin/ShakeMapGUI.java
+++ trunk/plugins/ShakeMapPlugin/src/org/scec/vtk/plugins/ShakeMapPlugin/ShakeMapGUI.java
@@ -1,13 +1,17 @@
 package org.scec.vtk.plugins.ShakeMapPlugin;
 
+import java.awt.GridLayout;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.io.File;
 import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import org.scec.vtk.drawingTools.DefaultLocationsGUI.PresetLocationGroup;
 import org.scec.vtk.main.Info;
 import org.scec.vtk.plugins.PluginActors;
 import org.scec.vtk.plugins.ShakeMapPlugin.Component.ShakeMap;
@@ -20,25 +24,50 @@ public class ShakeMapGUI extends JPanel implements ItemListener{
 
 
 	private static final long serialVersionUID = 1L;
+	
+	String dataPath = Info.getMainGUI().getCWD()+File.separator+"data/ShakeMapPlugin"; //path to directory with local folders
 
 	private JPanel shakeMapLibraryPanel;
-	JCheckBox checkBox;
+//	JCheckBox checkBox;
+	ArrayList<JCheckBox> defaultList; //for the local files in ShakeMapPlugin directory
 	ArrayList<ShakeMap> shakeMapsList;
 	PluginActors pluginActors;
-	ShakeMap shakeMap;
+//	ShakeMap shakeMap;
 	private ArrayList<vtkActor> actorList;
 	
 	public ShakeMapGUI(PluginActors pluginActors) {
 		shakeMapLibraryPanel = new JPanel();
-		checkBox = new JCheckBox("Chino Hills");
-		checkBox.addItemListener(this);
-		shakeMapLibraryPanel.add(checkBox);
+		shakeMapLibraryPanel.setLayout(new GridLayout(0,2)); //2 per row
+		
+//		checkBox = new JCheckBox("Chino Hills");
+//		checkBox.addItemListener(this);
+//		shakeMapLibraryPanel.add(checkBox);
+		defaultList = new ArrayList<JCheckBox>();
 		this.pluginActors = pluginActors;
 		this.add(shakeMapLibraryPanel);
 		shakeMapsList = new ArrayList<ShakeMap>();
 		actorList = new ArrayList<>();
 		
-		shakeMap = new ShakeMap(Info.getMainGUI().getCWD()+File.separator+"data/ShakeMapPlugin/Extra/colors.cpt");
+//		shakeMap = new ShakeMap(Info.getMainGUI().getCWD()+File.separator+"data/ShakeMapPlugin/Extra/colors.cpt");
+		
+		// Check to make sure it exists
+		File dataDirectory = new File(dataPath);
+		if (dataDirectory.isDirectory()) {
+			// List files in the directory and process each
+			File files[] = dataDirectory.listFiles();
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].isFile()) {
+					String tempName = files[i].getName();
+					JCheckBox tempCheckbox = new JCheckBox(tempName);
+					tempCheckbox.setName(tempName);
+					tempCheckbox.addItemListener(this);
+					shakeMapLibraryPanel.add(tempCheckbox);
+					defaultList.add(tempCheckbox); //add the JCheckBox to the list
+					shakeMapsList.add(null); //for now, initialize to null
+					actorList.add(null);
+				}
+			}
+		}
 		
 	}
 
@@ -55,27 +84,22 @@ public class ShakeMapGUI extends JPanel implements ItemListener{
 	public void itemStateChanged(ItemEvent e) {
 		// TODO Auto-generated method stub
 		Object src = e.getSource();
-		if(src == checkBox)
-		{
-			if (e.getStateChange()==ItemEvent.SELECTED) {
-				if(!shakeMapsList.contains(shakeMap))
-				{
-					shakeMap.loadFromFileToGriddedGeoDataSet(Info.getMainGUI().getCWD()+File.separator+"data/ShakeMapPlugin/Chino_Hills.txt");
-					shakeMapsList.add(shakeMap);
-					shakeMap.setActor(shakeMap.builtPolygonSurface());
-					actorList.add(shakeMap.getActor());
-					pluginActors.addActor(shakeMap.getActor());
-				}
-				else
-				{
-					shakeMapsList.get(0).getActor().SetVisibility(1);
-				}
+		for(int i=0; i<defaultList.size(); i++){
+			if(src == defaultList.get(i)){
+				if (e.getStateChange()==ItemEvent.SELECTED) {
+					if(shakeMapsList.get(i) == null){
+						ShakeMap shakeMap = new ShakeMap(Info.getMainGUI().getCWD()+File.separator+"data/ShakeMapPlugin/Extra/colors.cpt");
+						shakeMap.loadFromFileToGriddedGeoDataSet(Info.getMainGUI().getCWD()+File.separator+"data/ShakeMapPlugin/" + defaultList.get(i).getName());
+						shakeMap.setActor(shakeMap.builtPolygonSurface());
+						actorList.set(i, shakeMap.getActor());
+						pluginActors.addActor(shakeMap.getActor());
+						shakeMapsList.set(i, shakeMap);
+					}else
+						shakeMapsList.get(i).getActor().SetVisibility(1);
+				}else
+					shakeMapsList.get(i).getActor().SetVisibility(0);
+				Info.getMainGUI().updateRenderWindow();
 			}
-			else
-			{
-				shakeMapsList.get(0).getActor().SetVisibility(0);
-			}
-			Info.getMainGUI().updateRenderWindow();
 		}
 	}
 
