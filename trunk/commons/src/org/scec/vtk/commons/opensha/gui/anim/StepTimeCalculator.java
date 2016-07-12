5da7cd3d48f0c1baedf271692394d44dfba34c07
kmilner <kmilner@25aa0b20-aa2b-420e-afc3-456cccd12aca>
2016-07-12 20:53:46 +0000

    major updates to animation capabilities for time based animations. now allows for fading out of simulator events
    git-svn-id: http://source.usc.edu/svn/scec_vdo_vtk@163 25aa0b20-aa2b-420e-afc3-456cccd12aca
    
    Former-commit-id: a90592810e7da2ccfad864b339e5c6f6ee147d3a

--- trunk/commons/src/org/scec/vtk/commons/opensha/gui/anim/StepTimeCalculator.java
+++ trunk/commons/src/org/scec/vtk/commons/opensha/gui/anim/StepTimeCalculator.java
@@ -2,8 +2,51 @@ package org.scec.vtk.commons.opensha.gui.anim;
 
 public interface StepTimeCalculator {
 	
-	public int getStepForTime(int prevStep, long milis);
+	/**
+	 * @param millis into the animation, which is <= duration of current animation (NOT ABSOLUTE TIME FOR TIME BASED)
+	 * @return step for the given time in the animation
+	 */
+	public int getStepForAnimTime(long millis);
 	
-	public long getTimeUntil(long milis, int step);
+	/**
+	 * @param secs into the animation, which is <= duration of current animation (NOT ABSOLUTE TIME FOR TIME BASED)
+	 * @return step for the given time in the animation
+	 */
+	public int getStepForAnimTime(double secs);
+	
+	/**
+	 * @param prevStep
+	 * @param millis into the animation, which is <= duration of current animation (NOT ABSOLUTE TIME FOR TIME BASED)
+	 * @return step for the given time in the animation
+	 */
+	public int getStepForAnimTime(int prevStep, long millis);
+	
+	/**
+	 * @param prevStep
+	 * @param secs into the animation, which is <= duration of current animation (NOT ABSOLUTE TIME FOR TIME BASED)
+	 * @return step for the given time in the animation
+	 */
+	public int getStepForAnimTime(int prevStep, double secs);
+	
+	/**
+	 * Convert a time from the animation time frame to absolute time
+	 * @param animTimeSecs
+	 * @return
+	 */
+	public double getAbsoluteTime(double animTimeSecs);
+	
+	/**
+	 * Convert a time from absolute time frame to animation time
+	 * @param absoluteTimeSecs
+	 * @return
+	 */
+	public double getAnimTime(double absoluteTimeSecs);
+	
+	/**
+	 * @param milis current time in the animation (NOT ABSOLUTE TIME FOR TIME BASED)
+	 * @param step
+	 * @return animation time until the next step
+	 */
+	public long getAnimTimeUntil(long milis, int step);
 
 }
