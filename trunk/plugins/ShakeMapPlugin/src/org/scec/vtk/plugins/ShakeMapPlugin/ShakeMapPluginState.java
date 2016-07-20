81fe567cf76f447da4e79234cdf59b109c40e830
kishan <kishan@25aa0b20-aa2b-420e-afc3-456cccd12aca>
2016-07-20 21:24:00 +0000

    Completed the load function, so now statefulness works with timeline
    git-svn-id: http://source.usc.edu/svn/scec_vdo_vtk@202 25aa0b20-aa2b-420e-afc3-456cccd12aca

--- trunk/plugins/ShakeMapPlugin/src/org/scec/vtk/plugins/ShakeMapPlugin/ShakeMapPluginState.java
+++ trunk/plugins/ShakeMapPlugin/src/org/scec/vtk/plugins/ShakeMapPlugin/ShakeMapPluginState.java
@@ -12,21 +12,28 @@ import org.scec.vtk.main.Info;
 import org.scec.vtk.plugins.PluginState;
 import org.scec.vtk.plugins.ShakeMapPlugin.Component.ShakeMap;
 
+import vtk.vtkActor;
+
 public class ShakeMapPluginState implements PluginState{
 	
 	private ShakeMapGUI parent;
 
-
 	private ArrayList<String> filePath;
 	private ArrayList<Double> transparency;
+	private ArrayList<Integer> visibility;
 	private ArrayList<String> dispName;
+	private ArrayList<ShakeMap> shakeMaps;
+	private ArrayList<Integer> selectedIndexes;
 	
 	ShakeMapPluginState(ShakeMapGUI parent)
 	{
 		this.parent = parent;
-		filePath = new ArrayList<>();
-		transparency = new ArrayList<>();
-		dispName = new ArrayList<>();
+		filePath = new ArrayList<String>();
+		transparency = new ArrayList<Double>();
+		visibility = new ArrayList<Integer>();
+		dispName = new ArrayList<String>();
+		shakeMaps = new ArrayList<ShakeMap>();
+		selectedIndexes = new ArrayList<Integer>();
 	}
 
 	void copyLatestCatalogDetails()
@@ -35,35 +42,74 @@ public class ShakeMapPluginState implements PluginState{
 		filePath.clear();
 		transparency.clear();
 		dispName.clear();
+		selectedIndexes.clear();
 
+		int checkBoxIndex = 0;
 		for (JCheckBox box : parent.getCheckBoxList())
 		{
 			filePath.add(box.getName());
 			dispName.add(box.getName());
+			checkBoxIndex++;
 		}
 		
+		int shakeIndex = 0;
 		for(ShakeMap shake: parent.getShakeMapsList()){
-			if(shake != null) 
+			if(shake != null){ 
 				transparency.add(shake.getActor().GetProperty().GetOpacity());
+				visibility.add(shake.getActor().GetVisibility());
+				shakeMaps.add(shake);
+				selectedIndexes.add(shakeIndex);
+			}
+			else{
+				transparency.add(null);
+				visibility.add(0);
+				shakeMaps.add(null);
+			}
+			shakeIndex++;
 		}
 	}
 
 	@Override
 	public void load() {
 		// call methods to update based on the properties captured //might also want to put swing invoke and wait
-		int i=0;
-		for (JCheckBox surf : parent.getCheckBoxList())
-		{
-//			String fileName = surf.getName();
-			
-//			surf.setDisplayName(dispName.get(i));
-//			System.out.println(transparency.get(i));
-//			System.out.println(visibility.get(i));
-//			parent.setTransparency(surf, transparency.get(i));
-//			parent.setVisibility(surf,i,visibility.get(i));
-//			Info.getMainGUI().updateRenderWindow();
-//			i++;
+		
+		//try #1
+//		int i=0;
+//		int checkBoxIndex = 0;
+//		for (JCheckBox box : parent.getCheckBoxList())
+//		{
+//			if(box.isSelected()){
+//				System.out.println("Selected indicies are " + selectedIndexes);
+//				System.out.println("CheckBoxIndex is " + checkBoxIndex);
+//				System.out.println("size of transparency list is " + transparency.size());
+//				if(transparency.get(checkBoxIndex) != null)
+//					parent.getShakeMapsList().get(checkBoxIndex).getActor().GetProperty().SetOpacity(transparency.get(checkBoxIndex));
+//				i++;
+//			}
+//			checkBoxIndex++;
+//		}
+		
+		//try #2
+		System.out.println("Load start");
+		System.out.println("Selected index is " + selectedIndexes);
+		System.out.println("Parent ShakeMaps list is " + parent.getShakeMapsList());
+		System.out.println("Visiblity list is " + visibility);
+//		for(int i: selectedIndexes){
+//			shakeMaps.get(i).getActor().SetVisibility(visibility.get(i));
+//			shakeMaps.get(i).getActor().GetProperty().SetOpacity(transparency.get(i));
+//		}
+		for(int i=0; i<parent.getShakeMapsList().size(); i++){		
+			if(parent.getShakeMapsList().get(i) != null){
+				if(selectedIndexes.isEmpty())
+					break;
+				parent.getShakeMapsList().get(i).getActor().SetVisibility(visibility.get(i));
+			}
 		}
+		for(int i: selectedIndexes){
+			shakeMaps.get(i).getActor().GetProperty().SetOpacity(transparency.get(i));
+		}
+		System.out.println("Load end");		
+		
 	}
 
 	private void createElement(Element stateEl) {
