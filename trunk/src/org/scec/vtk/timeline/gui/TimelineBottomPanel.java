b52957edea17256ff40680988f23610f1a70fd03
kmilner <kmilner@25aa0b20-aa2b-420e-afc3-456cccd12aca>
2017-05-10 20:36:24 +0000

    animator/renderer bug fixes
    git-svn-id: http://source.usc.edu/svn/scec_vdo_vtk@296 25aa0b20-aa2b-420e-afc3-456cccd12aca
    
    Former-commit-id: 6779650c6abab76ba8be05392957fb6a0d5d35ae

--- trunk/src/org/scec/vtk/timeline/gui/TimelineBottomPanel.java
+++ trunk/src/org/scec/vtk/timeline/gui/TimelineBottomPanel.java
@@ -2,6 +2,7 @@ package org.scec.vtk.timeline.gui;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.text.DecimalFormat;
 
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
@@ -18,9 +19,11 @@ public class TimelineBottomPanel extends JPanel implements AnimationTimeListener
 	private JProgressBar playProgress;
 	private JProgressBar renderProgress;
 	
+	private static final String playbackText = "Playback";
 	private static final String renderingText = "Rendering";
 	private static final String finalizeText = "Finalizing Render";
 	private static final String renderDoneText = "Render Complete";
+	private boolean rendering = false;
 	
 	private double maxTime;
 	
@@ -33,7 +36,7 @@ public class TimelineBottomPanel extends JPanel implements AnimationTimeListener
 		
 		maxTime = timeline.getMaxTime();
 		
-		playProgress = buildProgress(play_min, play_max, "Playback", Color.BLUE);
+		playProgress = buildProgress(play_min, play_max, playbackText, Color.BLUE);
 		renderProgress = buildProgress(0, 1, "", Color.RED);
 		
 		add(playProgress);
@@ -48,37 +51,52 @@ public class TimelineBottomPanel extends JPanel implements AnimationTimeListener
 		progress.setForeground(color);
 		return progress;
 	}
+	
+	private static DecimalFormat timeDF = new DecimalFormat("0.0 s");
 
 	@Override
 	public void animationTimeChanged(double curTime) {
+		if (!rendering) {
+			renderProgress.setString("");
+			renderProgress.setValue(0);
+		}
 		double fract = curTime/maxTime;
 		int p = (int)Math.round(fract*play_max);
 		playProgress.setValue(p);
+		playProgress.setString(playbackText+": "+timeDF.format(curTime));
 	}
 
 	@Override
 	public void animationBoundsChanged(double maxTime) {
 		this.maxTime = maxTime;
 	}
+	
+	private static DecimalFormat percentDF = new DecimalFormat("0 %");
 
 	@Override
 	public void frameProcessed(int index, int count) {
+		rendering = true;
 		// index is 0-based
-		renderProgress.setString(renderingText+" Frame "+(index+1)+"/"+count);
+		index++;
+		double fract = (double)index/(double)count;
+		renderProgress.setString(renderingText+": Frame "+index+"/"+count+" ("+percentDF.format(fract)+")");
 		if (count != renderProgress.getMaximum())
 			renderProgress.setMaximum(count);
-		renderProgress.setValue(index+1);
+		renderProgress.setValue(index);
 	}
 
 	@Override
 	public void finalizeStarted() {
+		rendering = true;
 		renderProgress.setIndeterminate(true);
 		renderProgress.setString(finalizeText);
 	}
 
 	@Override
 	public void finalizeProgress(int current, int total) {
-		renderProgress.setString(finalizeText+" "+(current+1)+"/"+total);
+		rendering = true;
+		double fract = (double)current/(double)total;
+		renderProgress.setString(finalizeText+": "+(current+1)+"/"+total+" ("+percentDF.format(fract)+")");
 		if (total != renderProgress.getMaximum())
 			renderProgress.setMaximum(total);
 		renderProgress.setValue(current+1);
@@ -86,6 +104,7 @@ public class TimelineBottomPanel extends JPanel implements AnimationTimeListener
 
 	@Override
 	public void finalizeCompleted() {
+		rendering = false;
 		renderProgress.setString(renderDoneText);
 		renderProgress.setIndeterminate(false);
 		renderProgress.setValue(renderProgress.getMaximum());
