15ae87e843f616a1637504f5ed617c1e7093bd28
kmilner <kmilner@25aa0b20-aa2b-420e-afc3-456cccd12aca>
2017-07-10 22:26:35 +0000

    now does all calculations in fractional seconds, dealing in milliseconds (expressed as longs) was causing rounding errors
    git-svn-id: http://source.usc.edu/svn/scec_vdo_vtk@326 25aa0b20-aa2b-420e-afc3-456cccd12aca

--- trunk/commons/src/org/scec/vtk/commons/opensha/gui/anim/TimeBasedCalc.java
+++ trunk/commons/src/org/scec/vtk/commons/opensha/gui/anim/TimeBasedCalc.java
@@ -22,24 +22,11 @@ public class TimeBasedCalc implements StepTimeCalculator {
 		double maxTime = minTime + absoluteDuration;
 		Preconditions.checkState(maxTime > minTime, "the time difference for the min and max step must be > 0");
 		multiplier = duration / absoluteDuration;
-	}
-	
-	@Override
-	public synchronized int getStepForAnimTime(long milis) {
-		int step = getStepForAnimTime(prevStep, milis);
-		prevStep = step;
-		return step;
+		if (D) System.out.println("Created a new TimeBasedCalc with minTime="+minTime+", multiplier="+multiplier);
 	}
 
 	@Override
-	public int getStepForAnimTime(int prevStep, long milis) {
-		double secs = milis / 1000d;
-		if (D) System.out.println("TimeBasedCalc: getting step for time: "+milis+" (= " + secs + " secs)");
-		return getStepForAnimTime(prevStep, secs);
-	}
-
-	@Override
-	public synchronized int getStepForAnimTime(double secs) {
+	public synchronized int getStepForAnimTimeSecs(double secs) {
 		int step = getStepForAnimTime(prevStep, secs);
 		prevStep = step;
 		return step;
@@ -47,10 +34,12 @@ public class TimeBasedCalc implements StepTimeCalculator {
 	
 	@Override
 	public int getStepForAnimTime(int prevStep, double secs) {
-		if (D) System.out.println("TimeBasedCalc: Getting step for anim time: "+secs);
+		if (D) System.out.println("TimeBasedCalc: Getting step for anim time: "+secs+", prevStep="+prevStep);
 		double stepTime;
-		if (anim.getTimeForStep(prevStep) > secs)
+		if (getAnimTime(anim.getTimeForStep(prevStep)) > secs) {
 			prevStep = 0;
+			if (D) System.out.println("TimeBasedCalc: resetting search to beginning");
+		}
 		int step = prevStep;
 		for (int i=step; i<anim.getNumSteps(); i++) {
 			// this is absolute time
@@ -70,17 +59,18 @@ public class TimeBasedCalc implements StepTimeCalculator {
 	}
 
 	@Override
-	public long getAnimTimeUntil(long milis, int step) {
+	public double getAnimTimeUntil(double secs, int step) {
 		double absTime = anim.getTimeForStep(step);
 		double aninTime = getAnimTime(absTime);
 		if (D) System.out.println("TimeBasedCalc getTimeUntil: step "+step+" is at "+aninTime);
-		long milisToSleep = (long)(aninTime * 1000l) - milis;
-		if (D) System.out.println("Should sleep for "+milisToSleep+" milis");
-		return milisToSleep;
+		double secsToSleep = aninTime - secs;
+		if (D) System.out.println("Should sleep for "+secsToSleep+" secs");
+		return secsToSleep;
 	}
 
 	@Override
 	public double getAbsoluteTime(double animTimeSecs) {
+		if (D) System.out.println("TimeBasedCalc getAbsoluteTime: "+minTime+" + ("+animTimeSecs+"/"+multiplier+")");
 		return minTime + (animTimeSecs/multiplier);
 	}
 
