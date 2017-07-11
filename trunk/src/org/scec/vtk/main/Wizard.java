8fe51f28f70290215efced1c4222d4517274be59
ekala <ekala@25aa0b20-aa2b-420e-afc3-456cccd12aca>
2017-07-11 16:58:51 +0000

    MainGui( mainframe size updated, plugin frame size updated, wizframe pop up updated: size,added Do not show option, skip when closing frame, infoIcon button)
    git-svn-id: http://source.usc.edu/svn/scec_vdo_vtk@330 25aa0b20-aa2b-420e-afc3-456cccd12aca
    
    Former-commit-id: ea88d7aeeb2db3ffddd69381da125309afb1abc7

--- trunk/src/org/scec/vtk/main/Wizard.java
+++ trunk/src/org/scec/vtk/main/Wizard.java
@@ -6,8 +6,11 @@ import java.awt.event.ActionListener;
 import org.scec.vtk.main.MainMenu;
 
 import javax.swing.*;
+
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 
 @SuppressWarnings("serial")
 public class Wizard extends JPanel {
@@ -17,6 +20,7 @@ public class Wizard extends JPanel {
    public Wizard(final MainMenu mainMenu, final MainGUI mainGUI) {
 	   JPanel mainPanel = new JPanel();
 	   mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+//	   mainPanel.setSize(370, 130);
 
 	  JLabel titleLabel = new JLabel(TITLE_TEXT);
 	   titleLabel.setFont(titleLabel.getFont().deriveFont(Font.BOLD,
@@ -24,10 +28,13 @@ public class Wizard extends JPanel {
 
 	   FlowLayout layout = new FlowLayout();
 	   layout.setAlignment(FlowLayout.CENTER);
+	   
 	   JPanel southBtnPanel = new JPanel(layout);
+	  southBtnPanel.setLayout(new GridLayout(2,1,10,10));
+	   
 	   
 	   //Open an existing file using function from mainMenu
-	   JButton openButton = new JButton("Open a project");
+	   JButton openButton = new JButton("Open Existing Project");
 	   openButton.addActionListener(new ActionListener() {
 		      public void actionPerformed(ActionEvent ae) {
 		          mainMenu.open();
@@ -38,7 +45,7 @@ public class Wizard extends JPanel {
 		    });
 
 	   //Create (save) a file using function from mainMenu
-	   JButton createButton = new JButton("Create a project");
+	   JButton createButton = new JButton("Create a New Project");
 	   createButton.addActionListener(new ActionListener() {
 		      public void actionPerformed(ActionEvent ae) {
 		    	  mainMenu.save();
@@ -47,9 +54,24 @@ public class Wizard extends JPanel {
 		      }
 		    });
 	   
+	   JCheckBox checkBox = new JCheckBox("Do not show me this again");
+	   checkBox.setLayout(new FlowLayout());
+	   
+	   checkBox.addActionListener(new ActionListener() {
+		      public void actionPerformed(ActionEvent ae) {
+//		    	  mainMenu.save();
+//		    	  close frame after selection has been made
+		          mainGUI.wizFrame.setVisible(false);
+	
+		      }
+		    });
+	   
+	   
 	   southBtnPanel.add(openButton);
 	   southBtnPanel.add(createButton);
-	        
+	   southBtnPanel.add(checkBox, FlowLayout.RIGHT);
+	   
+	   
       JPanel titlePanel = new JPanel();
       titlePanel.add(titleLabel);
       mainPanel.add(titlePanel);
