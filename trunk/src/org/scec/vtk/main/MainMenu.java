c72689e3466f17e29f1c8389f5de3c5bc4cc797a
agnarvaez <agnarvaez@25aa0b20-aa2b-420e-afc3-456cccd12aca>
2017-07-24 18:18:36 +0000

    User Guide
    git-svn-id: http://source.usc.edu/svn/scec_vdo_vtk@397 25aa0b20-aa2b-420e-afc3-456cccd12aca

--- trunk/src/org/scec/vtk/main/MainMenu.java
+++ trunk/src/org/scec/vtk/main/MainMenu.java
@@ -721,14 +721,35 @@ public class MainMenu implements ActionListener, ItemListener{
 		 */
 		
 		else if(eventSource == tutorial) {
-			helpFrame = new JFrame("SCEC VDO User Guide");
-			Help helpGUI = new Help();
-			JScrollPane sp = new JScrollPane(helpGUI);
-		    helpFrame.getContentPane().add(sp);
-		    helpFrame.setSize(550, 500);
-		    helpFrame.setLocationRelativeTo(null);
-		    helpFrame.setVisible(true);
-		    helpFrame.setAlwaysOnTop(true);
+			File file = new File("userguide_HTML/userGuide.html");
+			String filepath = (file.getAbsolutePath());
+			String url = filepath.replace("\\", "/");
+				String os = System.getProperty("os.name").toLowerCase();
+			    Runtime rt = Runtime.getRuntime();
+				try{
+				    if (os.indexOf( "win" ) >= 0) {
+				        rt.exec( "rundll32 url.dll,FileProtocolHandler " + url);
+				    }
+				    else if (os.indexOf( "mac" ) >= 0) {
+
+				        rt.exec( "open " + url);
+			            }
+				    else if (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0) {
+				        String[] browsers = {"firefox", "mozilla", "epiphany", "konqueror",
+				       			             "netscape","opera","links","seamonkey", "galeon", "kazehakase","lynx"};
+				        StringBuffer cmd = new StringBuffer();
+				        for (int i=0; i<browsers.length; i++)
+				            cmd.append( (i==0  ? "" : " || " ) + browsers[i] +" \"" + url + "\" ");
+				        rt.exec(new String[] { "sh", "-c", cmd.toString() });
+				        } 
+			            else {
+			                return;
+			           }
+			       }
+				catch (Exception e1){
+				    return;
+			       }
+			      return;
 			}
 		else if(eventSource == wizardActivation){
 			
