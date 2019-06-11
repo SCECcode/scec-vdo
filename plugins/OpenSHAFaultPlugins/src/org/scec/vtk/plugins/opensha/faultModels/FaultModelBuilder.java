47e1a04025a4ce598163358feb526fce45e83c36
Kevin Milner <kmilner@usc.edu>
2019-06-11 12:47:00 -0700

    fixed FM plugin, now uses cached XML files by default instead of DB

--- plugins/OpenSHAFaultPlugins/src/org/scec/vtk/plugins/opensha/faultModels/FaultModelBuilder.java
+++ plugins/OpenSHAFaultPlugins/src/org/scec/vtk/plugins/opensha/faultModels/FaultModelBuilder.java
@@ -2,6 +2,8 @@ package org.scec.vtk.plugins.opensha.faultModels;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 
@@ -17,16 +19,17 @@ import org.scec.vtk.commons.opensha.tree.FaultCategoryNode;
 import org.scec.vtk.commons.opensha.tree.FaultSectionNode;
 import org.scec.vtk.commons.opensha.tree.builders.MultiDBBuilder;
 
+import scratch.UCERF3.enumTreeBranches.FaultModels;
+
 public class FaultModelBuilder extends MultiDBBuilder {
 	
 //	private ConnectionPointsDisplayPanel connsDisplay;
 
 	public FaultModelBuilder() {
 		super();
-//		this.connsDisplay = connsDisplay;
 	}
 
-	public FaultModelBuilder(String defaultDB) {
+	public FaultModelBuilder(DB_Source defaultDB) {
 		super(defaultDB);
 	}
 
@@ -36,56 +39,114 @@ public class FaultModelBuilder extends MultiDBBuilder {
 		
 //		connsDisplay.setDB(db);
 		
-		FaultModelSummaryDB_DAO faultModelSummaryDB_DAO = new FaultModelSummaryDB_DAO(db);
-		FaultModelDB_DAO faultModelDB_DAO = new FaultModelDB_DAO(db);
-		PrefFaultSectionDataDB_DAO faultSectionDB_DAO = new PrefFaultSectionDataDB_DAO(db);
-		
-		// cache everything
-		faultSectionDB_DAO.getAllFaultSectionPrefData();
-		
-		ArrayList<FaultModelSummary> summaries = faultModelSummaryDB_DAO.getAllFaultModels();
-//		
-		ArrayList<ArrayList<Integer>> sectionsList = new ArrayList<ArrayList<Integer>>();
-		
-		for (FaultModelSummary fm : summaries) {
-			sectionsList.add(faultModelDB_DAO.getFaultSectionIdList(fm.getFaultModelId()));
-		}
-		
-		for (int i=0; i<summaries.size(); i++) {
-			FaultModelSummary fm = summaries.get(i);
-			ArrayList<Integer> sections = sectionsList.get(i);
+		if (db == null) {
+			HashSet<Integer> common = null;
 			
-			ArrayList<Integer> common = new ArrayList<Integer>();
-			ArrayList<Integer> unique = (ArrayList<Integer>)sections.clone();
+			FaultModels[] fms = { FaultModels.FM3_1, FaultModels.FM3_2 };
+			List<List<FaultSectionPrefData>> sectsList = new ArrayList<>();
 			
-			for (int j=0; j<summaries.size(); j++) {
-				if (i == j)
-					continue;
-				ArrayList<Integer> otherSections = sectionsList.get(j);
-				for (int otherID : otherSections) {
-					if (sections.contains(otherID) && !common.contains(otherID)) {
-						common.add(otherID);
-						unique.remove((Integer)otherID);
-					}
-				}
+			for (FaultModels fm : fms) {
+				List<FaultSectionPrefData> sects = fm.fetchFaultSections();
+				
+				List<Integer> ids = new ArrayList<>();
+				for (FaultSectionPrefData sect : sects)
+					ids.add(sect.getSectionId());
+				
+				if (common == null)
+					common = new HashSet<>(ids);
+				else
+					common.retainAll(ids);
+				
+				sectsList.add(sects);
 			}
 			
-			FaultCategoryNode fmNode = new FaultCategoryNode(fm.getFaultModelName() + " ("+fm.getFaultModelId()+")");
+			for (int f = 0; f < fms.length; f++) {
+				FaultModels fm = fms[f];
+				List<FaultSectionPrefData> sects = sectsList.get(f);
+				
+				FaultCategoryNode fmNode = new FaultCategoryNode(fm.getName());
+				
+				List<FaultSectionNode> commonNodes = new ArrayList<>();
+				List<FaultSectionNode> uniqueNodes = new ArrayList<>();
+				
+				for (FaultSectionPrefData sect : sects) {
+					PrefDataSection fault = new PrefDataSection(sect);
+					
+					// add it to the tree
+					FaultSectionNode faultNode = new FaultSectionNode(fault);
+					if (common.contains(sect.getSectionId()))
+						commonNodes.add(faultNode);
+					else
+						uniqueNodes.add(faultNode);
+				}
+				// we do it this way to sort them before adding
+				Collections.sort(commonNodes, new NamedComparator());
+				Collections.sort(uniqueNodes, new NamedComparator());
+				
+				FaultCategoryNode uniqueNode = new FaultCategoryNode("Unique to this model");
+				for (FaultSectionNode faultNode : uniqueNodes)
+					uniqueNode.add(faultNode);
+				FaultCategoryNode commonNode = new FaultCategoryNode("Common other models");
+				for (FaultSectionNode faultNode : commonNodes)
+					commonNode.add(faultNode);
+				
+				fmNode.add(uniqueNode);
+				fmNode.add(commonNode);
+				
+				root.add(fmNode);
+			}
+		} else {
+			FaultModelSummaryDB_DAO faultModelSummaryDB_DAO = new FaultModelSummaryDB_DAO(db);
+			FaultModelDB_DAO faultModelDB_DAO = new FaultModelDB_DAO(db);
+			PrefFaultSectionDataDB_DAO faultSectionDB_DAO = new PrefFaultSectionDataDB_DAO(db);
 			
-			FaultCategoryNode uniqueNode = new FaultCategoryNode("Unique to this model");
-			addAll(uniqueNode, unique, faultSectionDB_DAO);
-			fmNode.add(uniqueNode);
+			// cache everything
+			faultSectionDB_DAO.getAllFaultSectionPrefData();
 			
-			FaultCategoryNode commonNode = new FaultCategoryNode("Common other models");
-			common.removeAll(unique);
-			addAll(commonNode, common, faultSectionDB_DAO);
-			fmNode.add(commonNode);
+			ArrayList<FaultModelSummary> summaries = faultModelSummaryDB_DAO.getAllFaultModels();
+//			
+			ArrayList<ArrayList<Integer>> sectionsList = new ArrayList<ArrayList<Integer>>();
 			
-			root.add(fmNode);
+			for (FaultModelSummary fm : summaries) {
+				sectionsList.add(faultModelDB_DAO.getFaultSectionIdList(fm.getFaultModelId()));
+			}
+			
+			for (int i=0; i<summaries.size(); i++) {
+				FaultModelSummary fm = summaries.get(i);
+				ArrayList<Integer> sections = sectionsList.get(i);
+				
+				ArrayList<Integer> common = new ArrayList<Integer>();
+				ArrayList<Integer> unique = (ArrayList<Integer>)sections.clone();
+				
+				for (int j=0; j<summaries.size(); j++) {
+					if (i == j)
+						continue;
+					ArrayList<Integer> otherSections = sectionsList.get(j);
+					for (int otherID : otherSections) {
+						if (sections.contains(otherID) && !common.contains(otherID)) {
+							common.add(otherID);
+							unique.remove((Integer)otherID);
+						}
+					}
+				}
+				
+				FaultCategoryNode fmNode = new FaultCategoryNode(fm.getFaultModelName() + " ("+fm.getFaultModelId()+")");
+				
+				FaultCategoryNode uniqueNode = new FaultCategoryNode("Unique to this model");
+				addAll(uniqueNode, unique, faultSectionDB_DAO);
+				fmNode.add(uniqueNode);
+				
+				FaultCategoryNode commonNode = new FaultCategoryNode("Common other models");
+				common.removeAll(unique);
+				addAll(commonNode, common, faultSectionDB_DAO);
+				fmNode.add(commonNode);
+				
+				root.add(fmNode);
+			}
 		}
 	}
 	
-	private static void addAll(FaultCategoryNode fmNode, ArrayList<Integer> secIDs,
+	private static void addAll(FaultCategoryNode fmNode, List<Integer> secIDs,
 			PrefFaultSectionDataDB_DAO faultSectionDB_DAO) {
 		ArrayList<FaultSectionNode> faultNodes = new ArrayList<FaultSectionNode>();
 		for (int secID : secIDs) {
