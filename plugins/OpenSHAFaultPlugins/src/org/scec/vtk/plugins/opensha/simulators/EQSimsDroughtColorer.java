af9d8af71d6374852ce699ea7c36d60c9b4d582a
Kevin Milner <kmilner@usc.edu>
2019-07-02 14:58:28 -0700

    new drought calculation procedures, and jar for FAST team

--- plugins/OpenSHAFaultPlugins/src/org/scec/vtk/plugins/opensha/simulators/EQSimsDroughtColorer.java
+++ plugins/OpenSHAFaultPlugins/src/org/scec/vtk/plugins/opensha/simulators/EQSimsDroughtColorer.java
@@ -15,11 +15,14 @@ import org.opensha.commons.param.event.ParameterChangeEvent;
 import org.opensha.commons.param.event.ParameterChangeListener;
 import org.opensha.commons.param.impl.DoubleParameter;
 import org.opensha.commons.param.impl.EnumParameter;
+import org.opensha.commons.param.impl.ParameterListParameter;
 import org.opensha.commons.util.ExceptionUtils;
 import org.opensha.commons.util.cpt.CPT;
-import org.opensha.sha.simulators.RSQSimEvent;
 import org.opensha.sha.simulators.SimulatorElement;
 import org.opensha.sha.simulators.SimulatorEvent;
+import org.scec.useit.forecasting.droughts.CatalogMinMagDroughtType;
+import org.scec.useit.forecasting.droughts.DroughtCalculator;
+import org.scec.useit.forecasting.droughts.DroughtType;
 import org.scec.vtk.commons.opensha.faults.AbstractFaultSection;
 import org.scec.vtk.commons.opensha.faults.colorers.CPTBasedColorer;
 import org.scec.vtk.commons.opensha.faults.faultSectionImpl.SimulatorElementFault;
@@ -32,18 +35,18 @@ public class EQSimsDroughtColorer extends CPTBasedColorer implements EQSimsEvent
 	private List<? extends SimulatorEvent> events;
 	
 	// we will eventually define droughts in multiple ways
-	private enum DroughtType {
-		STATEWIDE("No EQs Statewide M>X");
+	private enum DroughtTypes {
+		STATEWIDE(new CatalogMinMagDroughtType(7.5));
 
-		private String name;
-		private DroughtType(String name) {
-			this.name = name;
+		private DroughtType type;
+		private DroughtTypes(DroughtType type) {
+			this.type = type;
 		}
 
 		@Override
 		public String toString() {
 			// this is what the drop down menu will show
-			return name;
+			return type.getName();
 		}
 	}
 	
@@ -72,9 +75,9 @@ public class EQSimsDroughtColorer extends CPTBasedColorer implements EQSimsEvent
 	// allowing the user to adjust parameters within SCEC-VDO
 	private ParameterList params;
 
-	private EnumParameter<DroughtType> droughtTypeParam;
+	private EnumParameter<DroughtTypes> droughtTypeParam;
 	private DoubleParameter droughtDurationParam;
-	private DoubleParameter droughtMinMagParam;
+	private ParameterListParameter droughtParamsParam;
 	private EnumParameter<PlotType> plotTypeParam;
 	private DoubleParameter forecastMinMagParam;
 	private DoubleParameter forecastDurationParam;
@@ -84,7 +87,7 @@ public class EQSimsDroughtColorer extends CPTBasedColorer implements EQSimsEvent
 	 */
 	private Map<SimulatorElement, Double> tiProbs; // time independent rates
 	private Map<SimulatorElement, Double> droughtProbs; // event rates during drought
-	private Map<SimulatorElement, Double> stormProbs; // event rates afgter drought
+	private Map<SimulatorElement, Double> afterProbs; // event rates after drought
 
 	private static CPT getDefaultCPT() {
 		// default color palette, in log space
@@ -106,15 +109,9 @@ public class EQSimsDroughtColorer extends CPTBasedColorer implements EQSimsEvent
 		
 		// drought type
 		droughtTypeParam = new EnumParameter<>("Drought Type",
-				EnumSet.allOf(DroughtType.class), DroughtType.STATEWIDE, null);
+				EnumSet.allOf(DroughtTypes.class), DroughtTypes.STATEWIDE, null);
 		droughtTypeParam.addParameterChangeListener(this);
 		params.addParameter(droughtTypeParam);
-		
-		// drought min mag param
-		droughtMinMagParam = new DoubleParameter("Drought Min Mag", 0d, 10d);
-		droughtMinMagParam.setValue(7.5d);
-		droughtMinMagParam.addParameterChangeListener(this);
-		params.addParameter(droughtMinMagParam);
 		
 		// duration of the drought
 		droughtDurationParam = new DoubleParameter("Drought Duration", 0d, 1000);
@@ -122,6 +119,11 @@ public class EQSimsDroughtColorer extends CPTBasedColorer implements EQSimsEvent
 		droughtDurationParam.setValue(50);
 		droughtDurationParam.addParameterChangeListener(this);
 		params.addParameter(droughtDurationParam);
+		
+		// add all drought parameters
+		droughtParamsParam = new ParameterListParameter("Dought Parameters", droughtTypeParam.getValue().type.getParameters());
+		droughtParamsParam.addParameterChangeListener(this);
+		params.addParameter(droughtParamsParam);
 		
 		// drought type
 		plotTypeParam = new EnumParameter<>("Plot Type",
@@ -146,7 +148,7 @@ public class EQSimsDroughtColorer extends CPTBasedColorer implements EQSimsEvent
 	@Override
 	public String getName() {
 		// this is the name that shows up in the "Color Faults By" list
-		return "Drought/Storm Probabilities";
+		return "Drought Probabilities";
 	}
 
 	@Override
@@ -179,7 +181,7 @@ public class EQSimsDroughtColorer extends CPTBasedColorer implements EQSimsEvent
 			ret = droughtProbs.get(elem);
 			break;
 		case AFTER_DROUGHT:
-			ret = stormProbs.get(elem);
+			ret = afterProbs.get(elem);
 			break;
 
 		default:
@@ -212,43 +214,7 @@ public class EQSimsDroughtColorer extends CPTBasedColorer implements EQSimsEvent
 	private void clear() {
 		tiProbs = null;
 		droughtProbs = null;
-		stormProbs = null;
-	}
-	
-	private class Drought {
-		private double startTime;
-		private double endTime;
-		private double forecastEndTime;
-		
-		private List<SimulatorEvent> eventsDuringDrought;
-		private List<SimulatorEvent> eventsAfterDrought;
-		
-		public Drought(double startTime) {
-			this.startTime = startTime;
-			this.endTime = startTime + droughtDurationParam.getValue();
-			this.forecastEndTime = endTime + forecastDurationParam.getValue();
-			
-			eventsDuringDrought = new ArrayList<>();
-			eventsAfterDrought = new ArrayList<>();
-		}
-		
-		public double getDroughtEndTime() {
-			return endTime;
-		}
-		
-		public double getForecastEndTime() {
-			return forecastEndTime;
-		}
-		
-		public void addEvent(SimulatorEvent event) {
-			Preconditions.checkNotNull(event);
-			double time = event.getTimeInYears();
-			Preconditions.checkState(time >= startTime && time <= forecastEndTime);
-			if (time <= endTime)
-				eventsDuringDrought.add(event);
-			else
-				eventsAfterDrought.add(event);
-		}
+		afterProbs = null;
 	}
 	
 	/**
@@ -258,138 +224,17 @@ public class EQSimsDroughtColorer extends CPTBasedColorer implements EQSimsEvent
 		if (events == null || elements == null || events.size() < 2)
 			return;
 		System.out.println("Computing drought rates");
-		double totDurationYears = events.get(events.size()-1).getTimeInYears() - events.get(0).getTimeInYears();
-		Map<SimulatorElement, Integer> tiElementCounts = new HashMap<>();
+		DroughtCalculator calc = new DroughtCalculator(elements, events, droughtTypeParam.getValue().type);
 		
+		double droughtDuration = droughtDurationParam.getValue();
 		double forecastMinMag = forecastMinMagParam.getValue();
 		double forecastDuration = forecastDurationParam.getValue();
 		
-		List<Drought> droughts = new ArrayList<>();
-		Drought curDrought = null;
-		
-		double lastTime = 0d;
-		
-		for (SimulatorEvent event : events) {
-			double eventTime = event.getTimeInYears();
-			lastTime = eventTime;
-			boolean endsDrought = doesEventEndDrought(event);
-			if (endsDrought) {
-				// this event signifies the end of a drought
-				if (curDrought != null) {
-					// see if the current drought is long enough, and if so keep it
-					if (eventTime > curDrought.getDroughtEndTime()) {
-						// this drought was long enough, add it to the list
-						droughts.add(curDrought);
-					}
-					curDrought = null;
-				}
-			} else {
-				// this event does not end a drought
-				if (curDrought == null)
-					// weren't in a drought, start a new one
-					curDrought = new Drought(eventTime);
-				// have the current drought track this event
-				if (eventTime <= curDrought.getForecastEndTime())
-					curDrought.addEvent(event);
-			}
-			// now have all ended droughts track this event if it's during the post drought (storm) forecast window
-			for (Drought drought : droughts)
-				if (eventTime > drought.getDroughtEndTime() && eventTime <= drought.getForecastEndTime())
-					drought.addEvent(event);
-			
-			double mag = event.getMagnitude();
-			if (mag >= forecastMinMag) {
-				// this event is at or above our forecast min mag
-				// count it on every element which participated in the event
-				for (SimulatorElement element : event.getAllElements()) {
-					Integer prevCount = tiElementCounts.get(element);
-					if (prevCount == null)
-						prevCount = 0;
-					tiElementCounts.put(element, prevCount+1);
-				}
-			}
-		}
-		if (curDrought != null && lastTime >= curDrought.getForecastEndTime())
-			droughts.add(curDrought);
-		// cull any droughts where forecast end time > last event time
-		for (int i=droughts.size(); --i>=0;) {
-			if (droughts.get(i).getForecastEndTime() > lastTime)
-				droughts.remove(i);
-			else
-				break;
-		}
-		System.out.println("Found "+droughts.size()+" droughts");
-		
-		// now convert time independent counts to probabilities
-		tiProbs = new HashMap<>();
-		for (SimulatorElement elem : tiElementCounts.keySet()) {
-			int elemCount = tiElementCounts.get(elem);
-			double annualRate = (double)elemCount/totDurationYears;
-			// poisson probability: p = 1 - e^(-r*t)
-			double forecastProb = 1d - Math.exp(-annualRate*forecastDuration);
-			tiProbs.put(elem, forecastProb);
-		}
-		
-		// now compute drought and storm probs
-		int numDroughts = droughts.size();
-		if (numDroughts > 0) {
-			Map<SimulatorElement, Integer> droughtHitCounts = new HashMap<>();
-			Map<SimulatorElement, Integer> stormHitCounts = new HashMap<>();
-			for (Drought drought : droughts) {
-				// set of elements which had any event >= forecast mag during the drought
-				HashSet<SimulatorElement> droughtHits = new HashSet<>();
-				for (SimulatorEvent e : drought.eventsDuringDrought)
-					if (e.getMagnitude() >= forecastMinMag)
-						droughtHits.addAll(e.getAllElements());
-				// set of elements which had any event >= forecast mag during the forecast period after drought
-				HashSet<SimulatorElement> stormHits = new HashSet<>();
-				for (SimulatorEvent e : drought.eventsAfterDrought)
-					if (e.getMagnitude() >= forecastMinMag)
-						stormHits.addAll(e.getAllElements());
-				for (SimulatorElement e : droughtHits) {
-					Integer prevCount = droughtHitCounts.get(e);
-					if (prevCount == null)
-						prevCount = 0;
-					droughtHitCounts.put(e, prevCount+1);
-				}
-				for (SimulatorElement e : stormHits) {
-					Integer prevCount = stormHitCounts.get(e);
-					if (prevCount == null)
-						prevCount = 0;
-					stormHitCounts.put(e, prevCount+1);
-				}
-			}
-			// convert to probabilities
-			// prob = numHits/numDroughts
-			droughtProbs = new HashMap<>();
-			for (SimulatorElement e : droughtHitCounts.keySet()) {
-				int count = droughtHitCounts.get(e);
-				double prob = (double)count/(double)numDroughts;
-				droughtProbs.put(e, prob);
-			}
-			stormProbs = new HashMap<>();
-			for (SimulatorElement e : stormHitCounts.keySet()) {
-				int count = stormHitCounts.get(e);
-				double prob = (double)count/(double)numDroughts;
-				stormProbs.put(e, prob);
-			}
-		}
+		tiProbs = calc.getElementTimeIndependentProbs(forecastMinMag, forecastDuration);
+		droughtProbs = calc.getElementProbsDuringDrought(forecastMinMag, droughtDuration);
+		afterProbs = calc.getElementProbAfterDroughtYears(forecastMinMag, droughtDuration, forecastDuration);
 		
 		System.out.println("Done computing drought rates");
-	}
-	
-	/**
-	 * Return true of this event signifies the end of a drought, false otherwise
-	 * @param event
-	 * @return
-	 */
-	private boolean doesEventEndDrought(SimulatorEvent event) {
-		switch (droughtTypeParam.getValue()) {
-		case STATEWIDE:
-			return event.getMagnitude() >= droughtMinMagParam.getValue();
-		default:
-			throw new IllegalStateException("Drought type not yet implemented: "+droughtTypeParam.getValue());
-		}
 	}
 
 	@Override
@@ -400,6 +245,10 @@ public class EQSimsDroughtColorer extends CPTBasedColorer implements EQSimsEvent
 			// don't do this if only plot type was changed, as we calculate values
 			// for each plot type at once
 			clear();
+		}
+		if (e.getParameter() == droughtTypeParam) {
+			// update drought type parameters
+			droughtParamsParam.setValue(droughtTypeParam.getValue().type.getParameters());
 		}
 		// fire an event telling the plugin to redraw everything
 		fireColorerChangeEvent();
