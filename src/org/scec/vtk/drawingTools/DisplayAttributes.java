d16e9bc774810b11529e1dba395863dd61ceba5e
Gina Yang <ginayang@usc.edu>
2019-07-16 15:40:10 -0700

    Removed unimplemented strike/roll and angle settings in DisplayAttributes in drawingTools

--- src/org/scec/vtk/drawingTools/DisplayAttributes.java
+++ src/org/scec/vtk/drawingTools/DisplayAttributes.java
@@ -6,39 +6,28 @@ import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
-import javax.swing.JCheckBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
-
 
 public class DisplayAttributes extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = -1L;
 
 	protected JPanel textPanel = new JPanel();
-	protected JLabel flattenTextLabel = new JLabel("Flatten Text:");
 	protected JLabel textSizeLabel = new JLabel("Font Size:");
-	protected JLabel strikeAngleText = new JLabel("Strike Angle:");
-	protected JLabel rollAngleText = new JLabel("Roll Angle:");
 	protected JLabel latText = new JLabel("Lat:");
 	protected JLabel lonText = new JLabel("Lon:");
 	protected JLabel altText = new JLabel("Alt:");
 	protected JLabel coneSizeText = new JLabel("Cone pin's height:");
 	protected JLabel coneBaseRadiusText = new JLabel("Cone pin's base radius:");
-	protected JLabel rotateZText = new JLabel("Rotate Z:");
-	//    protected JLabel conSizeLabel = new JLabel("Cone Size:");
-	protected JCheckBox flattenTextCheckbox = new JCheckBox();
+
 	protected JFormattedTextField fontSizeField;
-	protected JFormattedTextField strikeAngleField;
 	protected JFormattedTextField coneHeightField;
 	protected JFormattedTextField coneBaseRadiusField;
-	protected JFormattedTextField rotateZField;
 	protected JFormattedTextField latField;
 	protected JFormattedTextField lonField;
 	protected JFormattedTextField altField;
-	protected JFormattedTextField rollAngleField;
 
 	public DisplayAttributes() {
 		//Font Size Field
@@ -64,15 +53,6 @@ public class DisplayAttributes extends JPanel implements ActionListener {
 		coneBaseRadiusField = new JFormattedTextField(rotateY);
 		coneBaseRadiusField.setText("5");
 
-		
-	
-//		DecimalFormat rotateZ = new DecimalFormat();
-//		rotateZ.setGroupingUsed(false);
-//		rotateZ.setMinimumIntegerDigits(1);
-//		rotateZ.setMaximumIntegerDigits(10);
-//		rotateZField = new JFormattedTextField(rotateZ);
-//		rotateZField.setText("90");
-
 		//lat Long and altitude
 		DecimalFormat lat = new DecimalFormat();
 		lat.setGroupingUsed(false);
@@ -95,22 +75,6 @@ public class DisplayAttributes extends JPanel implements ActionListener {
 		altField = new JFormattedTextField(alt);
 		altField.setText("0");
 
-		//Strike Angle Field
-		DecimalFormat decimalFormat = new DecimalFormat();
-		decimalFormat.setGroupingUsed(false);
-		decimalFormat.setMinimumIntegerDigits(1);
-		decimalFormat.setMaximumIntegerDigits(10);
-		strikeAngleField = new JFormattedTextField(decimalFormat);
-		strikeAngleField.setText("90");
-
-		//Roll Angle Field
-		DecimalFormat decimalFormat2 = new DecimalFormat();
-		decimalFormat2.setGroupingUsed(false);
-		decimalFormat2.setMinimumIntegerDigits(1);
-		decimalFormat2.setMaximumIntegerDigits(10);
-		rollAngleField = new JFormattedTextField(decimalFormat2);
-		rollAngleField.setText("0");
-
 		this.coneBaseRadiusField.setEnabled(false);
 		this.coneHeightField.setEnabled(false);
 		this.latField.setEnabled(false);
@@ -122,13 +86,6 @@ public class DisplayAttributes extends JPanel implements ActionListener {
 
 		JPanel enableLabelPanel = new JPanel();
 		enableLabelPanel.setLayout(new BoxLayout(enableLabelPanel, BoxLayout.Y_AXIS));
-		//enableLabelPanel.add(flattenTextLabel);
-
-
-		JPanel enableCheckboxPanel = new JPanel();
-		enableCheckboxPanel.setLayout(new BoxLayout(enableCheckboxPanel, BoxLayout.Y_AXIS));
-		//enableCheckboxPanel.add(flattenTextCheckbox);
-
 
 		JPanel translatePanel = new JPanel();
 		translatePanel.setLayout(new BoxLayout(translatePanel, BoxLayout.X_AXIS));
@@ -149,18 +106,6 @@ public class DisplayAttributes extends JPanel implements ActionListener {
 		rotatePanel.add(Box.createHorizontalGlue());
 		rotatePanel.add(coneBaseRadiusText);
 		rotatePanel.add(coneBaseRadiusField);
-		//rotatePanel.add(Box.createHorizontalGlue());
-		//rotatePanel.add(rotateZText);
-		//rotatePanel.add(rotateZField);
-
-		JPanel anglePanel = new JPanel();
-		anglePanel.setLayout(new BoxLayout(anglePanel, BoxLayout.X_AXIS));
-		anglePanel.add(strikeAngleText);
-		anglePanel.add(strikeAngleField);
-		anglePanel.add(Box.createHorizontalGlue());
-		anglePanel.add(rollAngleText);
-		anglePanel.add(rollAngleField);
-
 
 		JPanel textSettingPanel = new JPanel();
 		textSettingPanel.setLayout(new BoxLayout(textSettingPanel, BoxLayout.X_AXIS));
@@ -174,20 +119,11 @@ public class DisplayAttributes extends JPanel implements ActionListener {
 		labelPropertiesPanel.add(rotatePanel);
 		rotatePanel.add(Box.createVerticalGlue());
 		labelPropertiesPanel.add(textSettingPanel);
-		//anglePanel.add(Box.createVerticalGlue());
-		//labelPropertiesPanel.add(anglePanel);
 
 		this.add(Box.createVerticalGlue());
 		this.add(enableLabelPanel);
-		this.add(enableCheckboxPanel);
 		this.add(Box.createHorizontalGlue());
 		this.add(labelPropertiesPanel);
-
-		strikeAngleField.setPreferredSize(new Dimension(40, 20));
-		strikeAngleField.setMaximumSize(new Dimension(40, 20));
-
-		rollAngleField.setPreferredSize(new Dimension(40, 20));
-		rollAngleField.setMaximumSize(new Dimension(40, 20));
 
 		fontSizeField.setPreferredSize(new Dimension(40, 20));
 		fontSizeField.setMaximumSize(new Dimension(40, 20));
@@ -206,32 +142,6 @@ public class DisplayAttributes extends JPanel implements ActionListener {
 
 		coneBaseRadiusField.setPreferredSize(new Dimension(40, 20));
 		coneBaseRadiusField.setMaximumSize(new Dimension(40, 20));
-
-		//rotateZField.setPreferredSize(new Dimension(40, 20));
-		//rotateZField.setMaximumSize(new Dimension(40, 20));
-
-		flattenTextCheckbox.setSelected(false);
-		strikeAngleField.setEnabled(flattenTextCheckbox.isSelected());
-		rollAngleField.setEnabled(flattenTextCheckbox.isSelected());
-
-		flattenTextCheckbox.addActionListener(this);
-	}
-
-	public void actionPerformed(ActionEvent e) {
-		Object src = e.getSource();
-		if (src == flattenTextCheckbox) {
-			//Disable/Enabled strike dip rake
-			//strikeAngleField.setEnabled(flattenTextCheckbox.isSelected());
-			//rollAngleField.setEnabled(flattenTextCheckbox.isSelected());
-		}
-	}
-
-	public boolean isFlattened() {
-		return flattenTextCheckbox.isSelected();
-	}
-
-	public void setFlatten(boolean flatten) {
-		flattenTextCheckbox.setSelected(flatten);
 	}
 
 	public String getTextSize() {
@@ -240,38 +150,19 @@ public class DisplayAttributes extends JPanel implements ActionListener {
 			int fontTempSize = Integer.parseInt(fontSizeField.getText());
 			if (fontTempSize <= 0) throw new NumberFormatException();
 			fontSize = fontSizeField.getText();
-		} catch (NumberFormatException e2) {
+		} catch (NumberFormatException nfe) {
 			fontSizeField.setText("18");
 			fontSize = "18";
 		}
 		return fontSize;
 	}
 
-	public double getRollAngle(){
-		double roll;
-		try {
-			String strikeAngle = rollAngleField.getText();
-			roll = Double.parseDouble(strikeAngle);
-		} catch (NumberFormatException e2) {
-			rollAngleField.setText("0");
-			roll = 0;
-		}
-		return roll;
-	}
-
-	public double getStrikeAngle(){
-		double strike;
-		try {
-			String strikeAngle = strikeAngleField.getText();
-			strike = Double.parseDouble(strikeAngle);
-		} catch (NumberFormatException e2) {
-			strikeAngleField.setText("90");
-			strike = 90;
-		}
-		return strike;
-	}
-
-	public void setTestSize(String fontSize) {
+	public void setTextSize(String fontSize) {
 		fontSizeField.setText(fontSize);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// Auto-generated method stub
 	}
 }
