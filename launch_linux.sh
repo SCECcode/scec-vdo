ac587463a8c4bf74fa9b5053e4f4d3db4c97f197
Kevin Milner <kmilner@usc.edu>
2021-02-03 12:25:31 -0800

    added launch scripts for use outside of eclipse

---
+++ launch_linux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=vtkLibs/linux_64bit:$LD_LIBRARY_PATH
+
+if [[ -e $JAVA_HOME/lib/amd64 ]];then
+	export LD_LIBRARY_PATH=$JAVA_HOME/lib/amd64:$LD_LIBRARY_PATH
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR
+./.do_launch.sh vtkLibs/linux_64bit/vtk.jar
