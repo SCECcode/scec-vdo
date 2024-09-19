9c28b10752f81e01e11b21ea482df73977420c0d
Akash Bhatthal <akashbhatthal@gmail.com>
2024-09-18 21:34:40 -0700

    Move vtkLibs to LFS

--- launch_mac_osx.sh
+++ launch_mac_osx.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-export DYLD_LIBRARY_PATH=vtkLibs/macosx_64bit:$DYLD_LIBRARY_PATH
+if [ "$(uname -m)" == "arm64" ]; then
+  export DYLD_LIBRARY_PATH=vtkLibs/macosx_arm:$DYLD_LIBRARY_PATH
+else
+  export DYLD_LIBRARY_PATH=vtkLibs/macosx_64bit:$DYLD_LIBRARY_PATH
+fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $DIR
-./.do_launch.sh vtkLibs/macosx_64bit/vtk.jar
+./.do_launch.sh
+
