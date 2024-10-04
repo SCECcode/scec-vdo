84867ac9c536f786f19fc872287f94203255a2e0
Akash Bhatthal <akashbhatthal@gmail.com>
2024-10-04 02:22:42 -0700

    Create Windows batch launcher

--- launch_mac_osx.sh
+++ launch_mac_osx.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# When bundling for distribution, set JAVA_HOME to packaged JRE
 
 if [ "$(uname -m)" == "arm64" ]; then
   export DYLD_LIBRARY_PATH=vtkLibs/macosx_arm:$DYLD_LIBRARY_PATH
+  #export JAVA_HOME=jre/macosx_arm/Contents/Home
 else
   export DYLD_LIBRARY_PATH=vtkLibs/macosx_64bit:$DYLD_LIBRARY_PATH
+  #export JAVA_HOME=jre/macosx_64bit/Contents/Home
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
