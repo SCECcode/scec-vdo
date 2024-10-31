0847ab419bedd00e599b488f30e0a44fba828197
Akash Bhatthal <akashbhatthal@gmail.com>
2024-10-31 10:28:55 -0700

    Fix rpath for macOS

--- .edit_rpath.sh
+++ .edit_rpath.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# Update rpath in JNI libraries to search for dynamic libs in same directory.
+# This is necessary to load VTK when packaging releases.
 
 # Directory containing your JNI libs and dylibs
-LIB_DIR="dist/vtkLibs/macosx_arm/"
+LIB_DIR="$1"
 
 # Update each jnilib's rpath
 for jnilib in "$LIB_DIR"/*.jnilib; do
